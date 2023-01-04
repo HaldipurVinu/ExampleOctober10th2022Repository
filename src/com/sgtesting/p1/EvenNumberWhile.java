ed6d6ddc059fe3714c114320796b6b7d4b127516
HaldipurVinu <havinayak6@gmail.com>
2023-01-04 14:42:18 +0530

    EvenNumberDemo

---
+++ src/com/sgtesting/p1/EvenNumberWhile.java
@@ -0,0 +1,18 @@
+package com.sgtesting.p1;
+
+public class EvenNumberWhile {
+
+	public static void main(String[] args) {
+		int x=1;
+		
+	
+		while(x<=10)
+		{
+			 int num=x * 2;
+			System.out.println(num);
+			x++;
+		}
+
+	}
+
+}
